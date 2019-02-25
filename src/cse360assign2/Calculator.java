ba0e61f02b151f51e2f9960969bbe978f7f727ee
zachvice1 <40445865+zachvice1@users.noreply.github.com>
2019-02-24 20:06:38 -0700

    Update Calculator.java
    add, subtract, getTotal, mulitply, divide methods are written

--- src/cse360assign2/Calculator.java
+++ src/cse360assign2/Calculator.java
@@ -12,22 +12,28 @@ private int total;
 	}
 	
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
 	public void add (int value) {
+		total = total + value;
 		
 	}
 	
 	public void subtract (int value) {
-		
+		total = total - value;
 	}
 	
 	public void multiply (int value) {
-		
+		total = total * value;
 	}
 	
 	public void divide (int value) {
+		
+		if(value == 0) 
+			total = 0;
+		else 
+			total = total / value;
 		
 	}
 	
